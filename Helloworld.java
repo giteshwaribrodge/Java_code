d3421a48a117b1298e46b92749b80c9280c96e7c
giteshwaribrodge <106652728+giteshwaribrodge@users.noreply.github.com>
2022-06-01 13:13:09 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+/*program to display hello
+world for user*/
+//write a program
+
+import java.io.*;
+class Helloworld{
+	public static void main(String[] args){
+		int a=1;
+		int b=2;
+		int c;
+			c=a-b;
+		System.out.println("Helloworld" +c);
+	}
+
+}
